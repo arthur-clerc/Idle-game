0068386f69204768738fb607bab099b64e35e569
arthur-clerc <arthur.clerc@laplateforme.io>
2024-04-20 23:37:21 +0200

    display items to buy and clean code

--- store.js
+++ store.js
@@ -1,15 +1,12 @@
-let asideShop = document.querySelector("#asideShop");
-let asideInventory = document.querySelector("#asideInventory");
-let shop = document.getElementById('shop');
-let inventory = document.getElementById("inventory");
-let isOpenedInventory = false;
+const asideShop = document.querySelector("#asideShop");
+const shop = document.getElementById("shop");
 let isOpenedShop = false;
 
 shop.addEventListener("click", function () {
   if (isOpenedInventory === false && isOpenedShop === false) {
     asideShop.classList.toggle("open");
     isOpenedShop = true;
-  } else if (isOpenedInventory === true && isOpenedShop === false)  {
+  } else if (isOpenedInventory === true && isOpenedShop === false) {
     asideInventory.classList.toggle("open");
     isOpenedInventory = false;
     asideShop.classList.toggle("open");
@@ -20,34 +17,21 @@ shop.addEventListener("click", function () {
   }
 });
 
-inventory.addEventListener("click", function () {
-  if (isOpenedShop === false && isOpenedInventory === false) {
-    asideInventory.classList.toggle("open");
-    isOpenedInventory = true;
-  } else if (isOpenedShop === true && isOpenedInventory === false) {
-    asideShop.classList.toggle("open");
-    isOpenedShop = false;
-    asideInventory.classList.toggle("open");
-    isOpenedInventory = true;
-  } else if (isOpenedShop === false && isOpenedInventory === true) {
-    asideInventory.classList.toggle("open");
-    isOpenedInventory = false;
-  }
-});
-
-fetch('data.json')
-.then(response => response.json())
-.then(data => {
-  localStorage.setItem('shopItems', JSON.stringify(data));
-})
-.catch(error => console.error('Erreur lors du chargement des données :', error));
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    localStorage.setItem("shopItems", JSON.stringify(data));
+  })
+  .catch((error) =>
+    console.error("Erreur lors du chargement des données :", error)
+  );
 
 function fetchDataAndCreateElements() {
-  fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
       const armorSets = data.armorSet;
-      const gridContainerShop = document.querySelector('.grid-container-shop');
+      const gridContainerShop = document.querySelector(".grid-container-shop");
       let itemIdCounter = 1;
 
       for (const armorSetName in armorSets) {
@@ -56,23 +40,25 @@ function fetchDataAndCreateElements() {
           const piece = armorSet[pieceName];
           const src = piece.src;
           const price = piece.price;
-          const existingItem = document.querySelector(`#grid-item${itemIdCounter}`);
-          
+          const existingItem = document.querySelector(
+            `#grid-item${itemIdCounter}`
+          );
+
           if (!existingItem && src && price) {
-            const newItem = document.createElement('div');
+            const newItem = document.createElement("div");
             newItem.id = `grid-item${itemIdCounter}`;
-            newItem.className = 'shopItemIcon';
-            newItem.style.backgroundSize = 'cover';
+            newItem.className = "shopItemIcon";
+            newItem.style.backgroundSize = "cover";
             newItem.style.backgroundImage = `url("${src}")`;
-            newItem.style.padding = '0';
+            newItem.style.padding = "0";
             newItem.innerHTML = `
             <div style='display:flex; justify-content:center; align-items:center;'>
             <span style='display:flex; justify-content:center;color:white;'>${price}</span>
             <img class='goldPiecePng' src='assets/miscs/goldPiece.png'>
             </div>
             `;
-            
-            newItem.addEventListener('click', () => {
+
+            newItem.addEventListener("click", () => {
               handleItemClick(piece);
             });
 
@@ -82,25 +68,27 @@ function fetchDataAndCreateElements() {
         }
       }
     })
-    .catch(error => console.error('Error fetching data:', error));
+    .catch((error) => console.error("Error fetching data:", error));
 }
 
-function handleItemClick(piece) {
-  let boughtItems = JSON.parse(localStorage.getItem('boughtItems')) || {};
 
-  if (!boughtItems[piece.id]) {
-    boughtItems[piece.id] = {
-      src: piece.src,
-      price: piece.price,
-      buyReward: piece.buyReward,
-      goldPerSec: piece.goldPerSec
-    };
-    localStorage.setItem('boughtItems', JSON.stringify(boughtItems));
 
-    console.log('Element acheté:', piece);
-  } else {
-    console.log('Element déjà acheté:', piece);
-  }
-}
+// function handleItemClick(piece) {
+//   let boughtItems = JSON.parse(localStorage.getItem("boughtItems")) || {};
+
+//   if (!boughtItems[piece.id]) {
+//     boughtItems[piece.id] = {
+//       src: piece.src,
+//       price: piece.price,
+//       buyReward: piece.buyReward,
+//       goldPerSec: piece.goldPerSec,
+//     };
+//     localStorage.setItem("boughtItems", JSON.stringify(boughtItems));
+
+//     console.log("Element acheté:", piece);
+//   } else {
+//     console.log("Element déjà acheté:", piece);
+//   }
+// }
 
 fetchDataAndCreateElements();
